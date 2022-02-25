15cea374a541c41368709f8f8e8a08573d35c4b5
Vo Van Nghia <vanvnghia@gmail.com>
2022-02-25 02:11:40 +0100

    demo: rewrite

--- demo/main.cpp
+++ demo/main.cpp
@@ -1,42 +1,43 @@
-#include <SFML/Graphics.hpp>
-#include <chrono>
+#include <SFML/Graphics/CircleShape.hpp>
+#include <SFML/Graphics/Color.hpp>
+#include <SFML/Graphics/RenderWindow.hpp>
+#include <SFML/System/Clock.hpp>
+#include <SFML/System/Sleep.hpp>
+#include <SFML/Window/Event.hpp>
+#include <SFML/Window/VideoMode.hpp>
 #include <vector>
 
 #include "point.h"
 #include "space.h"
 
-#ifdef _WIN32
-#include <Windows.h>
-#else
-#include <unistd.h>
-#endif
+#define SPF 1 / 30
 
 int main() {
   int x_max = 2;
   int y_max = 2;
   int scale = 500;
   sf::RenderWindow window(sf::VideoMode(x_max * scale, y_max * scale), "QHM");
-  sleep(5);
+  window.setVerticalSyncEnabled(true);
 
-  auto points = std::vector<Point>{{1, 1, 0.1, 1}, {0.5, 1, 0.1, 1}};
-  auto space = Space{0, x_max, 0, y_max};
-  space.AddPoint(Point{1, 1, 0.1, 1}, 0, 0);
-  space.AddPoint(Point{0.5, 1, 0.1, 1}, 0.0000005, 0);
-  auto shapes = std::vector<sf::CircleShape>(points.size());
-  for (int i = 0; i < space.points.size(); ++i) {
-    auto point = space.points[i];
-    auto r = point.r(scale);
+  auto space = Space{0, (float)x_max, 0, (float)y_max};
+  space.AddPoint(Point{1, 1, 0.2, 1}, Coord{0.3, 0.35});
+  space.AddPoint(Point{1, 1.1, 0.1, 1}, Coord{0.9, 0});
+  space.AddPoint(Point{1.7, 1.1, 0.1, 1}, Coord{0.9, 0.75});
+  space.AddPoint(Point{1.75, 0.3, 0.1, 1}, Coord{1.9, 0.8});
+
+  auto shapes = std::vector<sf::CircleShape>(space.Size());
+  for (int i = 0; i < space.Size(); ++i) {
+    auto point = space[i];
+    auto r = point.r() * scale;
     shapes[i] = sf::CircleShape{r};
     shapes[i].setOrigin({r, r});
     shapes[i].setFillColor(sf::Color(100, 250, 50));
-    shapes[i].setPosition({point.x * scale, point.y * scale});
+    shapes[i].setPosition({point.coord.x * scale, point.coord.y * scale});
     shapes[i].setOutlineThickness(-r / 20);
     shapes[i].setOutlineColor(sf::Color(250, 0, 0));
   }
 
-  auto begin = std::chrono::steady_clock::now();
-  auto end = begin;
-
+  sf::Clock clock;
   while (window.isOpen()) {
     sf::Event event;
     while (window.pollEvent(event)) {
@@ -45,15 +46,11 @@ int main() {
     window.clear(sf::Color::Black);
     for (int i = 0; i < shapes.size(); ++i) {
       shapes[i].setPosition(
-          {space.points[i].x * scale, space.points[i].y * scale});
+          {space[i].coord.x * scale, space[i].coord.y * scale});
       window.draw(shapes[i]);
     }
     window.display();
-    end = std::chrono::steady_clock::now();
-    auto dt = std::chrono::duration_cast<std::chrono::microseconds>(end - begin)
-                  .count();
-    space.Update(dt);
-    begin = end;
+    space.Update(clock.restart().asSeconds());
   }
 
   return 0;
